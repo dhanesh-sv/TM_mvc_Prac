0988e475025703c3509080c729bd6649432509a1
dhanesh.s <dhanesh.sv@gmail.com>
2017-07-30 22:18:30 +0530

    doing state

--- TM_mvc/Controllers/StateController.cs
+++ TM_mvc/Controllers/StateController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TM_mvc.Models;
+using TM_mvc.ViewModels;
+using AutoMapper;
 
 namespace TM_mvc.Controllers
 {
     public class StateController : Controller
     {
+        TM_mvc_DBEntities db = new TM_mvc_DBEntities();
         // GET: State
         public ActionResult Index()
         {
+            List<state> obj = db.states.ToList();
             return View();
+        }
+
+        public ActionResult AddEditModal(long stateId=0)
+        {
+            stateViewModel obj = new stateViewModel();
+
+            Mapper.Initialize(cfg => cfg.CreateMap<state, stateViewModel>());
+
+            if (stateId == 0)
+            {
+                obj.country_id = 0;
+            }
+            else
+            {
+                state objstate = db.states.ToList().Where(x => x.state_id == stateId).FirstOrDefault();
+                obj = Mapper.Map<stateViewModel>(objstate);
+            }
+            obj.Countries = db.countries.ToList();
+            return PartialView("AddEditState",obj);
         }
     }
 }
---
+++ TM_mvc/ViewModels/stateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TM_mvc.Models;
+
+namespace TM_mvc.ViewModels
+{
+    public class stateViewModel
+    {
+        public long state_id { get; set; }
+        public string state_code { get; set; }
+        public string state_name { get; set; }
+        public long country_id { get; set; }
+        public bool is_active { get; set; }
+        public bool is_deleted { get; set; }
+        public long version { get; set; }
+
+        public IEnumerable<country> Countries { get; set; }
+    }
+}
